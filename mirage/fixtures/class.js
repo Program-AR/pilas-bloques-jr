573b5403e77a7277d088e33f847477499fdde4fc
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-09-29 02:13:52 -0300

    Comenzando a implementar el actor Alien.

--- mirage/fixtures/class.js
+++ mirage/fixtures/class.js
@@ -1,4 +1,5 @@
 export default [
   {id: 1, name: 'mono', className: 'Mono', blocks: ['controls_if_else', 'controls_whileUntil']},
   {id: 2, name: 'aceituna', className: 'Aceituna', blocks: ['controls_if']},
+  {id: 3, name: 'alien', className: 'Alien', blocks: ['green_color', 'set_color']},
 ];
