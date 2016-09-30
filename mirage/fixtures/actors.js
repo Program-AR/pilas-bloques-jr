1e41cbf27d7691152ec05b64dec318128f9c66a3
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-09-29 23:50:36 -0300

    Mejorando el código inicial del alien.

--- mirage/fixtures/actors.js
+++ mirage/fixtures/actors.js
@@ -1,3 +1,3 @@
 export default [
-  {id: 1, actorId: 'otro', classId: 3, x: 0, y: 0, sceneId: 1, workspaceXMLCode: '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="al_empezar_a_ejecutar" id="aF,i.tK-O(jDm1^GT4bP" deletable="false" x="90" y="40"></block></xml>'}
+  {id: 1, actorId: 'otro', classId: 3, x: 0, y: 0, sceneId: 1, workspaceXMLCode: '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="al_empezar_a_ejecutar" id="aF,i.tK-O(jDm1^GT4bP" deletable="false" movable="false" editable="false" x="90" y="40"><statement name="program"><block type="decir" id="q,n~yrvh;]r^GRQwz!5W"><value name="texto"><block type="text" id="ru,t_-c,n-rB+U]%i0T0"><field name="TEXT">Hola?</field></block></value></block></statement></block></xml>'}
 ];
