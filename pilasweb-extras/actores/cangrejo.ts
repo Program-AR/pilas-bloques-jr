6b0154349a7e8f34830a0b0caa6f1c94a420f0b1
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-10-13 21:56:52 -0300

    Agregando dos fondos nuevos y las animaciones nuevas del cangrejo.

--- pilasweb-extras/actores/cangrejo.ts
+++ pilasweb-extras/actores/cangrejo.ts
@@ -1,13 +1,13 @@
 class Cangrejo extends ActorAnimado {
 
   constructor(x, y) {
-    super(x, y, {grilla: 'actores/cangrejo.png', cantColumnas: 8, cantFilas: 3});
+    super(x, y, {grilla: 'actores/actor.CangrejoAnimado.png', cantColumnas: 8, cantFilas: 3});
 
     this.definirAnimacion("parado", [0, 1, 2, 3, 4, 5, 6, 7], 6, true);
     //this.definirAnimacion("correr", [9, 10, 11, 12, 13], 12);
     //this.definirAnimacion("recoger", [17, 18, 19, 20, 21, 21, 21, 19, 19], 6);
-    this.definirAnimacion("saltar", [20, 20], 6);
-    this.definirAnimacion("hablar", [17, 18, 19, 20, 21, 21, 21, 19, 19], 30);
+    this.definirAnimacion("saltar", [8], 6);
+    this.definirAnimacion("hablar", [14, 15, 22, 15], 5);
   }
 
 }
--- pilasweb-extras/actores/sandia.ts
+++ pilasweb-extras/actores/sandia.ts
@@ -1,7 +1,7 @@
 class Sandia extends ActorAnimado {
 
   constructor(x, y) {
-    super(x, y, {grilla: 'actores/sandia.png', cantColumnas: 5, cantFilas: 1});
+    super(x, y, {grilla: 'actores/actor.Sandia.png', cantColumnas: 5, cantFilas: 1});
 
     this.definirAnimacion("parado", [0], 6, true);
     this.definirAnimacion("comida", [1], 12);
