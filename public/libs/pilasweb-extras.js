728e3e61a434a6822b5bdb200ff2520b901eb1e9
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-12-05 14:23:20 -0300

    Mejorando la animación del cangrejo.

--- public/libs/pilasweb-extras.js
+++ public/libs/pilasweb-extras.js
@@ -43,7 +43,7 @@ var Cangrejo = (function (_super) {
         _super.call(this, x, y, { grilla: 'actores/actor.CangrejoAnimado.png', cantColumnas: 8, cantFilas: 3 });
         this.definirAnimacion("parado", [0, 1, 2, 3, 4, 5, 6, 7], 6, true);
         this.definirAnimacion("saltar", [8], 6);
-        this.definirAnimacion("hablar", [14, 15, 22, 15], 5);
+        this.definirAnimacion("hablar", [14, 15], 5);
     }
     return Cangrejo;
 }(ActorAnimado));
